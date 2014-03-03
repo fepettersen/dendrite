aeb1f87c620c36f4041388839d1b692d2c4b6b53
fepettersen <fredrik@fredrik-WorkStation.(none)>
2014-03-03 17:44:37 +0100

    think I have an ok idea

--- src/dendrite.py
+++ src/dendrite.py
@@ -1,7 +1,9 @@
 
 class Dendrite:
 	"""docstring for Dendrite"""
-	def __init__(self):
+	def __init__(self,mesh_points,max_spine_size = 5):
+		self.m = mesh_points
+		self.max_spine_contact_point = max_spine_size
 		PDE = Diffusion()
 	
 	def SetInitialCondition(self,U0):
@@ -10,20 +12,27 @@ class Dendrite:
 		PDE.Assemble(U0)
 		PDE.Precondition(PDE.A)
 
-	def AddSpine(self,x0,x1):
-		m0,n0 = self.MapAreaToIndex(x0,x1)
+	def AddSpine(self):
+		"""m is the total number of mesh-points on the dendrite"""
+		m0 = n0 = 0
+		m0 = random.random_integer(2,self.m-2)
+		while n0<m0 and (n0-m0)>self.max_spine_contact_point:
+			n0 = random.random_integer(0,self.m)
 		self.spines.append(Spine())
-
-	def MapAreaToIndex(self,x0,x1):
-		m = int(round(x0/dx))
-		n = int(round(x1/dx))
-		return m,n
+		self.indeces.append([m0,n0])
 
 	def Combine(self,tmp,m,n):
-		pass
+		tmp = self.U[m:n]
+
 
 	def Solve(self):
+		PDE.Source = self.Soma
 		self.U = PDE.solve(self.Up)
+		i = 0
 		for spine in self.spines:
+			if random.random()<self.spine_spike_probability_threshold:
+				spine.AddSpike()
 			tmp = spine.solve()
-			self.Combine(self.U,tmp,m,n)
+			m0 = self.indeces[i][0]
+			n0 = self.indeces[i][1]
+			self.Combine(self.U,tmp,m0,n0)
--- src/spine.py
+++ src/spine.py
@@ -1,8 +1,15 @@
-
+import sys, os
 class Spine:
 	"""docstring for Spine"""
 	def __init__(self):
-		pass
+		self.spike_probability = 0.003	# This number should probably be chosen more carefully
 		
 	def Solve(self):
+		# Make some reasonable way to solve RW stuff
+		for i in xrange(walkers):
+			#walk
+			pass
+
+	def AddSpike(self):
+		# Release/spawn some number of ions at Post Synaptic Density
 		pass
